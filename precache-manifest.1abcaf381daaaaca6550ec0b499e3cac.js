4f90d5c14e06e7212d72a3d8ce91abc06ed996b5
Swistek <63745881+Swistek92@users.noreply.github.com>
2021-07-22 15:18:07 +0200

    Add files via upload

---
+++ precache-manifest.1abcaf381daaaaca6550ec0b499e3cac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e61a1ee5a15d0a5352e",
+    "url": "/static/css/main.adaa56bd.chunk.css"
+  },
+  {
+    "revision": "4e61a1ee5a15d0a5352e",
+    "url": "/static/js/main.4e61a1ee.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "ec79daccafa45315dbf18ff19e5f48fb",
+    "url": "/static/media/header1.ec79dacc.jpg"
+  },
+  {
+    "revision": "da7832ec4d2c3c640df61d62dc4a9cf3",
+    "url": "/static/media/header3.da7832ec.jpg"
+  },
+  {
+    "revision": "4063f34f8f36dbda9e9875759a66aa62",
+    "url": "/static/media/header2.4063f34f.jpg"
+  },
+  {
+    "revision": "1eda7c924c3ec140149cf6a4211e8c53",
+    "url": "/index.html"
+  }
+];
